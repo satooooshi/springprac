f8e56e078193215e7afa3858d8ef6e87f5cd3cf1
aikawasatoshi <aikawasatoshi@aikawasatoshi-no-MacBook-Pro.local>
2018-02-08 20:20:59 +0800

    final ver. of promise-chai.

--- mycafe/lib/orders.js
+++ mycafe/lib/orders.js
@@ -1,95 +1,19 @@
 'use strict';
 
 var Q = require('q');
-
-//used for this.result in customer_display.js
-
-function asOrder(orderId) {
-
-  function totalPriceFor(items) {
-    return items.reduce(function (total, item) {
-      return total + item.beverage.price * item.quantity;
-    }, 0);
-  }
-//
-//Only avairable actions will be contained in here
-//Ex. empty order only cotains append-beverage action AVAIRABLE
-  function actionsFor(items) {
-
-    var actions = [
-      {
-        action: 'append-beverage',
-        target: orderId,
-        parameters: {
-          beverageRef: null,
-          quantity: 0
-        }
-      }
-    ];
-
-    items.forEach(function (item) {
-      //push into actions array[] above
-      //push() メソッドは、配列の末尾に 1 つ以上の要素を追加
-      actions.push({
-        action: 'edit-beverage',
-        target: orderId,
-        parameters: {
-          beverageRef: item.beverage.id,
-          newQuantity: item.quantity
-        }
-      });
-      actions.push({
-        action: 'remove-beverage',
-        target: orderId,
-        parameters: {
-          beverageRef: item.beverage.id
-        }
-      });
-    });
-    if (items.length > 0) {
-      actions.push({
-        action: 'place-order',
-        target: orderId
-      });
-    }
-    return actions;
-  }
-
-  return function (items) {
-    return {
-      items: items.slice(),
-      totalPrice: totalPriceFor(items),
-      actions: actionsFor(items)
-    };
-  };
-}
-
-module.exports = function (daos) {
-  var orderById = Q.denodeify(daos.order.byId),
-      messagesById = Q.denodeify(daos.message.byId),
-      updateMessages = Q.denodeify(daos.message.update);
-
-  function appendMessages(orderId) {
-    return function (order) {
-      return messagesById(orderId)
-          .then(function (messages) {
-            order.messages = messages.slice();
-            return updateMessages({
-              id: orderId,
-              data: []
-            });
-          }).then(function () {
-            //console.log(order);
-            return order;
-          });
-    };
-  }
-
-  return {
-    display: function (orderId) {
-      return orderById(orderId)
-          .then(asOrder(orderId))
-          .then(appendMessages(orderId));
-    }
-  };
-};
+   module.exports = function () {
+     return {
+       display: function (orderId) {
+         return Q.fulfill({
+           items: [],
+           totalPrice: 0,
+           actions: [
+             {
+               action: 'append-beverage',
+               target: orderId,
+               parameters: {
+                 beverageRef: null,
+                 quantity: 0 }
+               } ]
+             }); }
+           }; };
--- mycafe/test/customer_displays_order.js
+++ mycafe/test/customer_displays_order.js
@@ -5,6 +5,8 @@ expect = chai.expect,
 sinon = require('sinon'),
 orderSystemWith = require('../lib/orders');
 
+chai.use(require("chai-as-promised"));
+
    describe('Customer displays order', function () {
      beforeEach(function () {
        this.orderDAO = {
@@ -14,21 +16,51 @@ orderSystemWith = require('../lib/orders');
      });
      context('Given that the order is empty', function () {
        beforeEach(function () {
+         //
          //In Mocha, the this keyword will point to the same object throughout all the test
          this.orderId = 'some empty order id';
          //
          //withArgsでsinon.stub()のargsを指定
          //returnsで指定したargsのreturnを指定
          this.orderDAO.byId.withArgs(this.orderId).returns([]);
+         //
+         //The important point here is that
+         //the tests force us to model the shape of the object we return as a response.
          this.result = this.orderSystem.display(this.orderId);
        });
-
+       //
+       //define a response that will be a simple JSON object
+       //with the items, totalPrice, and actions fields.
        it('will show no order items', function () {
-         expect(this.result).to.have.property('items')
+         //
+         //relation between promise-mocha and promise-chai
+         //expect(this.result).to.have.property('items')
+         //.that.is.empty;
+         //===
+         //add the eventually chain and RETURN the resulting assertion
+         return expect(this.result).to.eventually.have.property('items')
          .that.is.empty;
        });
-       it('will show 0 as total price');
-       it('will only be possible to add a beverage');
+       it('will show 0 as total price', function(){
+         //
+         //expect(this.result).to.have.property('totalPrice')
+         //.that.is.equal(0);
+         //===
+         return expect(this.result).to.eventually.have.property('totalPrice')
+         .that.is.equal(0);
+       });
+       it('will only be possible to add a beverage', function(){
+         return expect(this.result).to.have.eventually.have.property('actions')
+        .that.is.deep.equal([
+          {
+          action: 'append-beverage',
+               target: orderId,
+               parameters: {
+                 beverageRef: null,
+                 quantity: 0 }
+          }
+        ]);
+       });
       });
       context('Given that the order contains beverages', function() {
        it('will show one item per beverage');
@@ -50,3 +82,15 @@ orderSystemWith = require('../lib/orders');
 //<ACTION> is the user operation represented by this feature,
 //and <ENTITY> is the main information entity that is affected by the feature action
 //
+
+
+function promiseFor(value) {
+     return Q.delay(1).then(function () {
+       return value;
+     });
+}
+   function failingPromiseWith(error) {
+     return Q.delay(1).then(function () {
+       throw error;
+     });
+}
